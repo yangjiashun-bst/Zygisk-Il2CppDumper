5cad41820a5fdf3fe9f0bb9f0b217f82581cb7bc
yangjiashun-bst <103926735+yangjiashun-bst@users.noreply.github.com>
2025-07-01 14:08:59 +0800

    1111

--- module/src/main/cpp/hack.cpp
+++ module/src/main/cpp/hack.cpp
@@ -31,7 +31,7 @@ void hack_start(const char *game_data_dir) {
         return;
 #endif
     for (int i = 0; i < 10; i++) {
-        void *handle = xdl_open("libil2cpp.so", 0);
+        void *handle = xdl_open("liblogic.so", 0);
         if (handle) {
             load = true;
             il2cpp_api_init(handle);
